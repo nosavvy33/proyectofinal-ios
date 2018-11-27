bc7168b506399e86ac2768d70492bb5ea574232d
leon <leon@leons-Mac.local>
2018-11-27 09:04:35 -0800

    Initial commit

---
+++ MiDoc/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MiDoc
+//
+//  Created by leon on 11/27/18.
+//  Copyright © 2018 leon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
